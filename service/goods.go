5a5fc9a6094c126ec0ab553e26b590e12e316551
liyuanwu <16619891743@163.com>
2023-03-27 23:31:55 +0800

    fix bug

--- service/goods.go
+++ service/goods.go
@@ -3,7 +3,7 @@ package service
 import "github.com/liyuanwu2020/msgo/rpc"
 
 type GoodsService struct {
-	Find func(args map[string]any) ([]byte, error) `msrpc:"Get,/goods/find"`
+	Find func(args map[string]any) ([]byte, error) `msrpc:"GET,/goods/find"`
 }
 
 func (s *GoodsService) Env() rpc.HttpConfig {
