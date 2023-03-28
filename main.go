e6bbb313304deb3e7421663ebc44f3c5dfedb49c
好好住-liyuanwu <liyuanwu@haohaozhu.com>
2023-03-28 19:02:30 +0800

    grpc demo

--- main.go
+++ main.go
@@ -1,10 +1,16 @@
 package main
 
 import (
+	"context"
+	"github.com/liyuanwu2020/goods/model"
 	"github.com/liyuanwu2020/msgo"
 	"github.com/liyuanwu2020/msgo/rpc"
+	"github.com/liyuanwu2020/order/api"
 	"github.com/liyuanwu2020/order/service"
+	"google.golang.org/grpc"
+	"google.golang.org/grpc/credentials/insecure"
 	"log"
+	"net/http"
 )
 
 func main() {
@@ -19,11 +25,34 @@ func main() {
 		//通过商品中心 查询商品信息
 		//http的调用的方式
 		//body, err := client.Get("http://127.0.0.1:9002/goods/find")
-		//改造目标
+		//改造目标 rpc 方式
 		body, err := client.Do("goods", "Find").(*service.GoodsService).Find(params)
-		log.Println(err)
-		log.Println(string(body))
+		if err != nil {
+			log.Println(err)
+		}
+		ctx.JSON(http.StatusOK, &model.Result{Code: 1000, Msg: "success from rpc", Data: string(body)})
 
 	})
+
+	group.Get("/findGrpc", func(ctx *msgo.Context) {
+		conn, err := grpc.Dial("localhost:9111", grpc.WithTransportCredentials(insecure.NewCredentials()))
+		if err != nil {
+			panic(err)
+		}
+		defer func(conn *grpc.ClientConn) {
+			err := conn.Close()
+			if err != nil {
+
+			}
+		}(conn)
+		rpcClient := api.NewGoodsApiClient(conn)
+		rsp, err := rpcClient.Find(context.TODO(), &api.GoodsRequest{})
+		if err != nil {
+			panic(err)
+		}
+
+		ctx.JSON(http.StatusOK, rsp)
+	})
+
 	engine.Run(":9003")
 }
