4b7efaeb989db371ee5394d2ccf9cb33d71294c6
好好住-liyuanwu <liyuanwu@haohaozhu.com>
2023-04-11 18:24:10 +0800

    rebuild msgo

---
+++ service/index.go
@@ -0,0 +1,17 @@
+package service
+
+import (
+	"github.com/liyuanwu2020/msgo/engine"
+	"time"
+)
+
+type Order struct {
+	Id int    `json:"id"`
+	No string `json:"no"`
+}
+
+func Route(ctx *engine.Context) {
+	time.Sleep(time.Millisecond * 123)
+	ctx.Logger.Info("执行顺序 main")
+	_ = ctx.Json(&Order{Id: 189485, No: "yjxnld"})
+}
